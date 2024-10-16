5f5877ec12f616d450c3392b1995d8a0daffeb97
yelkov <yelkovq@gmail.com>
2024-10-16 21:44:33 +0200

    feat(service): merge requests to PokemonData n create cacheManager

--- src/main/java/edu/badpals/pokeapi/service/CacheManager.java
+++ src/main/java/edu/badpals/pokeapi/service/CacheManager.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.badpals.pokeapi.model.Area;
 import edu.badpals.pokeapi.model.Pokemon;
+import edu.badpals.pokeapi.model.PokemonData;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,30 +16,25 @@ import java.util.Map;
 public class CacheManager {
     private static final String DIR_CACHE = "cache/";
 
-    public static void saveCache(Pokemon pokemon, List<Area> areas){
+    public static void saveCache(PokemonData data){
         try{
             ObjectMapper mapper = new ObjectMapper();
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(DIR_CACHE + pokemon.getName() + ".json"),pokemon);
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(DIR_CACHE + pokemon.getName() + "_areas.json"),areas);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(DIR_CACHE + data.getPokemon().getName() + ".json"),data);
         }catch (IOException e){
             System.out.println("Error saving cache.");
             e.printStackTrace();
         }
     }
 
-    public static Map<Pokemon,List<Area>> loadCache(String pokemonName){
-        Map<Pokemon,List<Area>> loadedCache = new HashMap<>();
+    public static PokemonData loadCache(String pokemonName){
         try{
             ObjectMapper mapper = new ObjectMapper();
-            Pokemon pokemon = mapper.readValue(new File(DIR_CACHE + pokemonName + ".json"), Pokemon.class);
-            List<Area> areas = mapper.readValue(new File(DIR_CACHE + pokemonName + "_areas.json"), new TypeReference<List<Area>>() {});
-            loadedCache.put(pokemon,areas);
-            return loadedCache;
+            return mapper.readValue(new File(DIR_CACHE + pokemonName + ".json"), PokemonData.class);
         } catch (IOException e) {
             System.out.println("Error loading cache. ");
             e.printStackTrace();
         }
-        return loadedCache;
+        return null;
     }
 
 }
