dcd4522468d3ab357a66f14200d882a31473d326
a23yelkovq <yelkovq@gmail.com>
2024-10-18 14:05:31 +0200

    feat(model): mapping image classes

---
+++ src/main/java/edu/badpals/pokeapi/model/Sprites.java
@@ -0,0 +1,23 @@
+package edu.badpals.pokeapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class Sprites implements Serializable {
+    private static final  long serialVersionUID = 1L;
+
+    @JsonProperty("front_default")
+    private String front_default;
+
+    public Sprites() {
+    }
+
+    public String getFront_default() {
+        return front_default;
+    }
+
+    public void setFront_default(String front_default) {
+        this.front_default = front_default;
+    }
+}
