99645a246c2c69a1c14da89d3e146753caeb8f36
yelkov <yelkovq@gmail.com>
2024-10-20 23:12:38 +0200

    doc(package-info): añadir documentacion a todos los paquetes

---
+++ src/main/java/edu/badpals/pokeapi/controller/package-info.java
@@ -0,0 +1,12 @@
+/**
+ *  Contiene las clases que manejan la interacción entre la vista y
+ *   el modelo, asegurando que la información se presente de manera
+ *   adecuada en la interfaz de usuario.
+ *
+ *
+ * @author  David Búa - @BuaTeijeiro
+ * @author  Yelko Veiga - @yelkov
+ *
+ * @version 1.0
+ */
+package edu.badpals.pokeapi.controller;
