980736bda6ac7b87da201a5f51b069bda7fa379e
yelkov <yelkovq@gmail.com>
2024-10-20 02:26:11 +0200

    doc(service): documentar clases paquete service

--- src/main/java/edu/badpals/pokeapi/service/APIPetitions.java
+++ src/main/java/edu/badpals/pokeapi/service/APIPetitions.java
@@ -15,11 +15,22 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class APIPetitions {
+    // URL base para obtener información sobre Pokémon
     private static final String POKEMON_URL = "https://pokeapi.co/api/v2/pokemon-species/";
+    // URL dividida en 2 partes para obtener información sobre las áreas donde se encuentra el Pokémon en el juego
     private static final String ENCOUNTERS_URL_BEGIN = "https://pokeapi.co/api/v2/pokemon/";
     private static final String ENCOUNTERS_URL_END = "/encounters";
+    // URL para obtener la imagen del Pokémon
     private static final String IMAGE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
+    /**
+     * Obtiene, a partir de varias llamadas a otros métodos, la información necesaria para construir
+     * un objeto PokemonData, que será el objeto que se manipule en memoria y se muestre al usuario.
+     *
+     * @param pokemonName El nombre del Pokémon a buscar.
+     * @return Un objeto PokemonData que contiene la información del Pokémon.
+     * @throws IOException Si hay un error al realizar las peticiones a la API.
+     */
     public static PokemonData getPokemonData(String pokemonName) throws IOException {
         Pokemon pokemon = askAPIforPokemon(pokemonName);
         List<Area> areas = askAPIforArea(pokemon.getId());
@@ -29,45 +40,72 @@ public class APIPetitions {
         return pokemonData;
     }
 
+    /**
+     * Realiza una petición a la API para obtener los datos básicos de un Pokémon.
+     *
+     * @param pokemonName El nombre del Pokémon a buscar.
+     * @return Un objeto Pokemon que representa al Pokémon solicitado.
+     * @throws IOException Si hay un error al realizar la petición.
+     */
     private static Pokemon askAPIforPokemon(String pokemonName) throws IOException{
         try{
             URL jsonURL = new URL(POKEMON_URL + pokemonName);
             ObjectMapper mapper = new ObjectMapper();
+            // Configurar el mapper para ignorar propiedades que no deseamos manejar
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
             Pokemon pokemon = mapper.readValue(jsonURL, Pokemon.class);
 
             return pokemon;
 
         } catch (MalformedURLException e) {
+            // Si hay errores en la formación de la URL, registrar un error
             ErrorLogger.saveErrorLog("The URL of pokemon is invalid");
         }
         return null;
     }
 
+    /**
+     * Realiza una petición a la API para obtener las áreas en las que se encuentra un Pokémon en el juego.
+     *
+     * @param pokemonID El ID del Pokémon para el cual se desean obtener las áreas de encuentro.
+     * @return Una lista de objetos Area representando las áreas de encuentro.
+     */
     private static List<Area> askAPIforArea(int pokemonID){
         try{
             URL jsonURL = new URL(ENCOUNTERS_URL_BEGIN + pokemonID + ENCOUNTERS_URL_END);
             ObjectMapper mapper = new ObjectMapper();
+            // Configurar el mapper para ignorar propiedades que no deseamos manejar
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
             List<Area> areas = mapper.readValue(jsonURL, new TypeReference<List<Area>>(){});
 
             return areas;
 
         } catch (MalformedURLException e) {
+            // Si hay errores en la formación de la URL, registrar un error
             ErrorLogger.saveErrorLog("The URL of area is invalid");
         }catch (IOException e) {
+            // Si hay errores deserializando las áreas, registrar un error
             ErrorLogger.saveErrorLog("Error reading areas from jsonURL.");
         }
         return null;
     }
 
+    /**
+     * Realiza una petición a la API para obtener la imagen de un Pokémon específico.
+     *
+     * @param pokemonName El nombre del Pokémon para el cual se desea obtener la imagen.
+     * @return Un objeto PokemonImage que representa la imagen del Pokémon.
+     * @throws IOException Si hay un error al realizar la petición.
+     */
     private static PokemonImage askAPIforImage(String pokemonName) throws IOException {
         try{
             URL jsonURL = new URL(IMAGE_URL + pokemonName);
             ObjectMapper mapper = new ObjectMapper();
+            // Configurar el mapper para ignorar propiedades que no deseamos manejar
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
             return mapper.readValue(jsonURL, PokemonImage.class);
         }catch (MalformedURLException e) {
+            // Si hay errores durante la petición, registrar un error
             ErrorLogger.saveErrorLog("The URL of image is invalid");
         }
         return null;
--- src/main/java/edu/badpals/pokeapi/service/CacheManager.java
+++ src/main/java/edu/badpals/pokeapi/service/CacheManager.java
@@ -18,42 +18,73 @@ import java.util.List;
 import java.util.Map;
 
 public class CacheManager {
+    // Directorio donde se almacenarán los archivos de caché
     private static final String DIR_CACHE = "cache/";
 
+    /**
+     * Guarda en caché los datos de un Pokémon y su imagen.
+     * Los datos del Pokémon se guardan en formato JSON y la imagen se descarga y guarda localmente como un archivo PNG.
+     *
+     * @param data Los datos del Pokémon en memoria que se desea almacenar en caché.
+     */
     public static void saveCache(PokemonData data){
         try{
             ObjectMapper mapper = new ObjectMapper();
+            // Guardar los datos del Pokémon en un archivo JSON
             mapper.writerWithDefaultPrettyPrinter().writeValue(new File(DIR_CACHE + data.getPokemon().getName() + ".json"),data);
 
+            // Descargar la imagen del Pokémon y guardarla en un archivo PNG
             URL imageURL = new URL(data.getPokemonImage().obtainImage());
             try(InputStream in = imageURL.openStream();){
                 Files.copy(in, Paths.get(DIR_CACHE + data.getPokemon().getName() + "_image.png"));
             }
 
         }catch (IOException e){
+            // Registra un error si falla al guardar en caché
             ErrorLogger.saveErrorLog("Error saving cache. Cache directory does not exist:" + DIR_CACHE);
         }
     }
 
+    /**
+     * Carga los datos desde caché de un Pokémon almacenado en un archivo JSON.
+     *
+     * @param pokemonName El nombre del Pokémon cuyos datos se desean cargar.
+     * @return El objeto PokemonData con los datos almacenados en caché.
+     * @throws IOException Si no se puede leer el archivo de caché.
+     */
     public static PokemonData loadCache(String pokemonName) throws IOException{
 
         ObjectMapper mapper = new ObjectMapper();
+        // Leer el archivo JSON y convertirlo en un objeto PokemonData
         return mapper.readValue(new File(DIR_CACHE + pokemonName + ".json"), PokemonData.class);
 
     }
 
+    /**
+     * Carga la ruta de la imagen guardada localmente en caché de un Pokémon.
+     *
+     * @param pokemonName El nombre del Pokémon cuya imagen se desea cargar.
+     * @return La ruta local de la imagen del Pokémon en formato de URL de archivo.
+     */
     public static String loadImageCache(String pokemonName){
         String pokemonImage = "file:" + DIR_CACHE + pokemonName + "_image.png";
         return pokemonImage;
     }
 
+    /**
+     * Elimina todos los archivos de caché almacenados en el directorio de caché.
+     * Si el directorio no existe, se registra un error.
+     */
     public static void deleteCache() {
         File cacheDirectory = new File(DIR_CACHE);
 
+        // Verificar si el directorio de caché existe y es un directorio
         if(cacheDirectory.exists() && cacheDirectory.isDirectory()){
 
+            // Obtener la lista de archivos dentro del directorio de caché
             File[] cacheFiles = cacheDirectory.listFiles();
             if (cacheFiles != null){
+                // Eliminar cada archivo dentro del directorio
                 for (File file : cacheFiles){
                     if (file.isFile()){
                         file.delete();
@@ -61,6 +92,7 @@ public class CacheManager {
                 }
             }
         }else{
+            // Si no existe el directorio de caché, registrar un error
             ErrorLogger.saveErrorLog("Error deleting cache. Cache directory does not exist: " + DIR_CACHE);
         }
     }
